156bc75fe0d9de62d0f5ebce81f84c367d56564c
Ram Natla <ramnatla@gmail.com>
2018-08-16 11:00:42 -0400

    Used a local newsAPI es5 version to fix minify errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import LinkList from './components/LinkList';
 import './App.css';
-import NewsAPI from './testApi.js'
+import NewsAPI from './newsAPI.js'
 
-//const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('4db68098d46f4ef89bb5fbc60e217d41');
-    // "newsapi": "^2.3.0",
 
 class App extends Component {
   constructor(){
